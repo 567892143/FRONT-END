0413bf99ef6a568cb8613a2a3c485ae37e0a5e48
567892143 <20211a04n5@bvrit.ac.in>
2025-03-22 10:38:10 +0530

    commit for header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category/category.component';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,CommonModule,CategoryComponent],
+  imports: [RouterOutlet,CommonModule,CategoryComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
+  {path:'', component:HeaderComponent},
   { path: 'category', component: CategoryComponent }
 ];
